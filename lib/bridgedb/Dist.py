plotskogwq/bridgedb

commit 34876cec5e9d7cc6cf6c1a8f37a5e33dd5a22c51

Scrub client information before logging

lib/bridgedb/Dist.py
@@ -8,6 +8,7 @@ This module has functions to decide which bridges to hand out to whom.
 
 import bridgedb.Bridges
 import bridgedb.Storage
+import bridgedb.Util as Util
 
 import logging
 import re
@@ -131,14 +132,15 @@ class IPBasedDistributor(bridgedb.Bridges.BridgeHolder):
            N -- the number of bridges to try to give back.
         """
         if not bridgeFilterRules: bridgeFilterRules=[]
-        logging.debug("getBridgesForIP(%s, %s, %s, %s" % (ip, epoch, N, bridgeFilterRules))
+        logging.debug("getBridgesForIP(%s, %s, %s, %s",
+                Util.logSafely(ip), epoch, N, bridgeFilterRules)
         if not len(self.splitter):
             logging.debug("bailing without splitter")
             return []
 
         area = self.areaMapper(ip)
 
-        logging.info("area is %s" % area)
+        logging.info("area is %s", Util.logSafely(area))
         
         key1 = ''
         pos = 0
@@ -154,8 +156,8 @@ class IPBasedDistributor(bridgedb.Bridges.BridgeHolder):
                                                       len(self.categories),
                                                       n)
                 bridgeFilterRules.append(g)
-                logging.info("category<%s>%s"%(epoch,area))
-                pos = self.areaOrderHmac("category<%s>%s"%(epoch,area))
+                logging.info("category<%s>%s", epoch, Util.logSafely(area))
+                pos = self.areaOrderHmac("category<%s>%s", epoch, area)
                 key1 = bridgedb.Bridges.get_hmac(self.splitter.key,
                                              "Order-Bridges-In-Ring-%d"%n) 
                 break;
@@ -364,15 +366,15 @@ class EmailBasedDistributor(bridgedb.Bridges.BridgeHolder):
         if lastSaw is not None and lastSaw + MAX_EMAIL_RATE >= now:
             if wasWarned:
                 logging.info("Got a request for bridges from %r; we already "
-                             "sent a warning. Ignoring.", emailaddress)
-                raise IgnoreEmail("Client was warned", emailaddress)
+                             "sent a warning. Ignoring.", Util.logSafely(emailaddress))
+                raise IgnoreEmail("Client was warned", Util.logSafely(emailaddress))
             else:
                 db.setWarnedEmail(emailaddress, True, now)
                 db.commit() 
 
             logging.info("Got a request for bridges from %r; we already "
                          "answered one within the last %d seconds. Warning.",
-                         emailaddress, MAX_EMAIL_RATE)
+                         Util.logSafely(emailaddress), MAX_EMAIL_RATE)
             raise TooSoonEmail("Too many emails; wait till later", emailaddress)
 
         # warning period is over

lib/bridgedb/EmailServer.py
@@ -25,6 +25,7 @@ import twisted.mail.smtp
 from zope.interface import implements
 
 import bridgedb.Dist
+import bridgedb.Util as Util
 from bridgedb.Dist import BadEmail, TooSoonEmail, IgnoreEmail
 from bridgedb.Filters import filterBridgesByIP6, filterBridgesByIP4
 from bridgedb.Filters import filterBridgesByTransport
@@ -99,11 +100,11 @@ def getMailResponse(lines, ctx):
         logging.info("Ignoring bad address on incoming email.")
         return None,None
     if not addrdomain:
-        logging.info("Couldn't parse domain from %r", clientAddr)
+        logging.info("Couldn't parse domain from %r", Util.logSafely(clientAddr))
     if addrdomain and ctx.cfg.EMAIL_DOMAIN_MAP:
         addrdomain = ctx.cfg.EMAIL_DOMAIN_MAP.get(addrdomain, addrdomain)
     if addrdomain not in ctx.cfg.EMAIL_DOMAINS:
-        logging.info("Unrecognized email domain %r", addrdomain)
+        logging.info("Unrecognized email domain %r", Util.logSafely(addrdomain))
         return None,None
     rules = ctx.cfg.EMAIL_DOMAIN_RULES.get(addrdomain, [])
     if 'dkim' in rules:
@@ -176,7 +177,7 @@ def getMailResponse(lines, ctx):
     # Handle rate limited email
     except TooSoonEmail, e:
         logging.info("Got a mail too frequently; warning %r: %s.",
-                     clientAddr, e)
+                     Util.logSafely(clientAddr), e)
 
         # Compose a warning email
         # MAX_EMAIL_RATE is in seconds, convert to hours
@@ -186,12 +187,12 @@ def getMailResponse(lines, ctx):
 
     except IgnoreEmail, e:
         logging.info("Got a mail too frequently; ignoring %r: %s.",
-                      clientAddr, e)
+                      Util.logSafely(clientAddr), e)
         return None, None 
 
     except BadEmail, e:
         logging.info("Got a mail from a bad email address %r: %s.",
-                     clientAddr, e)
+                     Util.logSafely(clientAddr), e)
         return None, None 
 
     if bridges:
@@ -251,7 +252,7 @@ def replyToMail(lines, ctx):
         response,
         d)
     reactor.connectTCP(ctx.smtpServer, ctx.smtpPort, factory)
-    logging.info("Sending reply to %r", sendToUser)
+    logging.info("Sending reply to %r", Util.logSafely(sendToUser))
     return d
 
 def getLocaleFromPlusAddr(address):

lib/bridgedb/HTTPServer.py
@@ -22,6 +22,7 @@ from twisted.web.util import redirectTo
 
 import bridgedb.Dist
 import bridgedb.I18n as I18n
+import bridgedb.Util as Util
 
 from recaptcha.client import captcha 
 from bridgedb.Raptcha import Raptcha
@@ -96,11 +97,11 @@ class CaptchaProtectedResource(twisted.web.resource.Resource):
                                         self.recaptchaPrivKey, remote_ip)
         if recaptcha_response.is_valid:
             logging.info("Valid recaptcha from %s. Parameters were %r",
-                    remote_ip, request.args)
+                    Util.logSafely(remote_ip), request.args)
             return self.resource.render(request)
         else:
             logging.info("Invalid recaptcha from %s. Parameters were %r",
-                         remote_ip, request.args)
+                         Util.logSafely(remote_ip), request.args)
             logging.info("Recaptcha error code: %s", recaptcha_response.error_code)
         return redirectTo(request.URLPath(), request)
 
@@ -209,8 +210,8 @@ class WebResource(twisted.web.resource.Resource):
                 request=bridgedb.Dist.uniformMap(ip)
                 ) for b in bridges) 
 
-        logging.info("Replying to web request from %s.  Parameters were %r", ip,
-                     request.args)
+        logging.info("Replying to web request from %s.  Parameters were %r",
+                     Util.logSafely(ip), request.args)
         if format == 'plain':
             request.setHeader("Content-Type", "text/plain")
             return answer

commit 06ce990ca9e47e4dedd017e74ba9810370865df9

HMAC isn't a logger, don't pass it args as if it is
Bug fix on #9237. Bug introduced in 34876cec

lib/bridgedb/Dist.py
@@ -157,7 +157,7 @@ class IPBasedDistributor(bridgedb.Bridges.BridgeHolder):
                                                       n)
                 bridgeFilterRules.append(g)
                 logging.info("category<%s>%s", epoch, Util.logSafely(area))
-                pos = self.areaOrderHmac("category<%s>%s", epoch, area)
+                pos = self.areaOrderHmac("category<%s>%s" % (epoch, area))
                 key1 = bridgedb.Bridges.get_hmac(self.splitter.key,
                                              "Order-Bridges-In-Ring-%d"%n) 
                 break;
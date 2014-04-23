plotskogwq/bridgedb

commit 20959f9cdd7eb1c613e6ba4538c92b85f7e17fb7

Add crypto.gpgSignMessage() utility function.

lib/bridgedb/crypto.py
@@ -347,6 +347,32 @@ def getGPGContext(cfg):
 
     return ctx
 
+def gpgSignMessage(gpgmeCtx, messageString, mode=None):
+    """Sign a **messageString** with a GPGME context.
+
+    :param gpgmeCtx: A ``gpgme.Context`` initialised with the appropriate
+        settings.
+    :param str messageString: The message to sign.
+    :param mode: The signing mode. (default: ``gpgme.SIG_MODE_CLEAR``)
+    :rtype: tuple
+    :returns: A 2-tuple of ``(signature, list)``, where:
+        * ``signature`` is the ascii-armored signature text.
+        * ``list`` is a list of ``gpgme.NewSignature``s.
+
+    .. warning:: The returned signature text and list *may* be empty, if no
+        signature was created.
+    """
+    if not mode:
+        mode = gpgme.SIG_MODE_CLEAR
+
+    msgFile = io.StringIO(unicode(messageString))
+    sigFile = io.StringIO()
+    sigList = gpgmeCtx.sign(msgFile, sigFile, mode)
+    sigFile.seek(0)
+    signature = sigFile.read()
+
+    return (signature, sigList)
+
 
 class SSLVerifyingContextFactory(ssl.CertificateOptions):
     """``OpenSSL.SSL.Context`` factory which does full certificate-chain and

lib/bridgedb/EmailServer.py
@@ -29,6 +29,7 @@ from bridgedb import Dist
 from bridgedb import I18n
 from bridgedb import safelog
 from bridgedb.crypto import getGPGContext
+from bridgedb.crypto import gpgSignMessage
 from bridgedb.Filters import filterBridgesByIP6
 from bridgedb.Filters import filterBridgesByIP4
 from bridgedb.Filters import filterBridgesByTransport

commit b84321acc5a6191628af403c2064ff1efce547db

Fix GPGME 'invalid argument' error while signing.
 * FIX error introduced in commit 20959f9c in branch for #11522
   fixes. The in-memory files for the message and the signature file for
   libgpgme need to be type io.BytesIO(buffer(message)), not
   io.StringIO(unicode()) like the rest of the email body files.

lib/bridgedb/crypto.py
@@ -343,8 +343,7 @@ def getGPGContext(cfg):
         ctx.signers = (bridgedbKey,)
 
         logging.debug("Testing signature created with GnuPG key...")
-        testMessage = "Testing 1 2 3"
-        signatureText, sigs = gpgSignMessage(ctx, testMessage)
+        signatureText, sigs = gpgSignMessage(ctx, "Testing 1 2 3")
 
         if not len(sigs) == 1:
             raise PythonicGpgmeError("Testing couldn't produce a signature "\
@@ -396,8 +395,8 @@ def gpgSignMessage(gpgmeCtx, messageString, mode=None):
     if not mode:
         mode = gpgme.SIG_MODE_CLEAR
 
-    msgFile = io.StringIO(unicode(messageString))
-    sigFile = io.StringIO()
+    msgFile = io.BytesIO(buffer(messageString))
+    sigFile = io.BytesIO()
     sigList = gpgmeCtx.sign(msgFile, sigFile, mode)
     sigFile.seek(0)
     signature = sigFile.read()